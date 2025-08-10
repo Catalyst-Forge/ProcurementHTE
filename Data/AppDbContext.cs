cc6f254220cbf9b660f75cbc1fa98057840d58b7
bryankazuro04 <mhdalana211201@gmail.com>
2025-08-10 15:09:01 +0700

    feat(db context and connection): create & Add DB Context and connecting DB
    Created DBContext class and configured database connection for entity management and data access
    
    BREAKING CHANGE: Need to change User & Role by using Identity

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using project_25_07.Models;
+
+namespace project_25_07.Data {
+  public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<string>(options) {
+    protected override void OnModelCreating(ModelBuilder builder) {
+      base.OnModelCreating(builder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+      base.OnConfiguring(optionsBuilder);
+    }
+  }
+}
