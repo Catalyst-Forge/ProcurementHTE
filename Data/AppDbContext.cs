6d9237a5fb94c293f1b8d33cc520c9383c42a7aa
bryankazuro04 <mhdalana211201@gmail.com>
2025-08-10 22:39:44 +0700

    feat(db context): implement FullName in User Entity by combining FirstName and LastName
    Added FullName property to the User Entity that concatenates FirstName and LastName for easier
    display and data handling

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,6 +6,8 @@ namespace project_25_07.Data {
   public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User, Role, string>(options) {
     protected override void OnModelCreating(ModelBuilder builder) {
       base.OnModelCreating(builder);
+
+      builder.Entity<User>().Property(u => u.FullName).HasComputedColumnSql("CONCAT([FirstName], ' ', [LastName])");
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
