549dc3f8c4e39a596c79147df20e22c64a035283
bryankazuro04 <mhdalana211201@gmail.com>
2025-10-13 17:09:00 +0700

    feat(number generator): add sequence number generator for Work Order or other numbering
    Implemented a sequence number generator to automatically create formatted IDs for column in table WO

---
+++ ProcurementHTE.Core/Utils/SequenceNumberGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace ProcurementHTE.Core.Utils
+{
+    public static class SequenceNumberGenerator
+    {
+        public static string NumId(string prefix, string? lastId)
+        {
+            var next = 1;
+            if (
+                !string.IsNullOrWhiteSpace(lastId)
+                && lastId.StartsWith(prefix, StringComparison.OrdinalIgnoreCase)
+            )
+            {
+                var numPart = lastId[prefix.Length..];
+                if (int.TryParse(numPart, out var lastNum))
+                {
+                    next = lastNum + 1;
+                }
+            }
+
+            return $"{prefix}{next.ToString("D6")}";
+        }
+    }
+}
