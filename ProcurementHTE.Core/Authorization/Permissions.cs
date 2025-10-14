9f81ed4220abc351852902c49f9262e3c1f0c3f7
bryankazuro04 <mhdalana211201@gmail.com>
2025-10-14 23:59:38 +0700

    feat(authorization permissions): create permissions for WorkOrder, Vendor, and document
    Implemented authorization permissions for WorkOrder, Vendor, and Document modules to enable more
    granular access control and role-based authorization

---
+++ ProcurementHTE.Core/Authorization/Permissions.cs
@@ -0,0 +1,28 @@
+﻿namespace ProcurementHTE.Core.Authorization
+{
+    public static class Permissions
+    {
+        public static class WO
+        {
+            public const string Read = "WO.Read";
+            public const string Create = "WO.Create";
+            public const string Edit = "WO.Edit";
+            public const string Delete = "WO.Delete";
+        }
+
+        public static class Vendor
+        {
+            public const string Read = "Vendor.Read";
+            public const string Create = "Vendor.Create";
+            public const string Edit = "Vendor.Edit";
+            public const string Delete = "Vendor.Delete";
+        }
+
+        public static class Doc
+        {
+            public const string Read = "Doc.Read";
+            public const string Upload = "Doc.Upload";
+            public const string Approve = "Doc.Approve";
+        }
+    }
+}
