dd4109cf6adc437f2d14d0f5b864bc6b8dd1b3a6
bryankazuro04 <mhdalana211201@gmail.com>
2025-10-17 22:17:32 +0700

    feat(authorization): create custom user claim principal
    Implemented a custom user claim principal to extend user identity with additional claims, enabling
    more flexible and granular authorization across the application

---
+++ ProcurementHTE.Core/Services/CustomUserClaimsPrincipalFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using ProcurementHTE.Core.Models;
+using System.Security.Claims;
+
+namespace ProcurementHTE.Core.Services {
+    public class CustomUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<User, Role> {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+        public CustomUserClaimsPrincipalFactory(UserManager<User> userManager, RoleManager<Role> roleManager, IOptions<IdentityOptions> optionsAccessor) : base(userManager, roleManager, optionsAccessor) {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(User user) {
+            var identity = await base.GenerateClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            foreach (var roleName in roles) {
+                var role = await _roleManager.FindByNameAsync(roleName);
+                if (role != null) {
+                    var roleClaims = await _roleManager.GetClaimsAsync(role);
+                    foreach (var claim in roleClaims) {
+                        identity.AddClaim(claim);
+                    }
+                }
+            }
+
+            return identity;
+        }
+    }
+}
