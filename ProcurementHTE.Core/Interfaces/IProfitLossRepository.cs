34a6d7dab90d8d8fc1f76551f6915a5bba1a6699
bryankazuro04 <mhdalana211201@gmail.com>
2025-10-17 09:24:29 +0700

    feat(core): create interfaces repository and service for ProfitLoss
    Added repository and service interfaces for the ProfitLoss model to define core data operations and
    business logic structure, ensuring better maintainability and scalability

---
+++ ProcurementHTE.Core/Interfaces/IProfitLossRepository.cs
@@ -0,0 +1,21 @@
+﻿using ProcurementHTE.Core.Models;
+
+namespace ProcurementHTE.Core.Interfaces
+{
+    public interface IProfitLossRepository
+    {
+        // Get Data
+        Task<IEnumerable<ProfitLoss>> GetAllAsync();
+        Task<ProfitLoss?> GetByIdAsync(string id);
+        Task<ProfitLoss?> GetByWorkOrderAsync(string woId);
+        Task<IEnumerable<ProfitLoss>> GetProfitLossByDateRangeAsync(
+            DateTime startDate,
+            DateTime endDate
+        );
+
+        // Transaction DB
+        Task<ProfitLoss> StoreProfitLossAsync(ProfitLoss pnl);
+        Task UpdateProfitLossAsync(ProfitLoss pnl);
+        Task DropProfitLossAsync(string id);
+    }
+}
