4f44402df345e8e4619f5f9cfd9808c88803853b
bryankazuro04 <mhdalana211201@gmail.com>
2025-10-17 09:45:29 +0700

    feat(viewmodels): create CRUD ViewModel for ProfitLoss
    Created a dedicated CRUD ViewModel for ProfitLoss to manage data transfer between views and
    controllers, improving structure, validation, and maintainability

---
+++ ProcurementHTE.Web/Models/ViewModels/DeleteProfitLossViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProcurementHTE.Web.Models.ViewModels {
+    public class DeleteProfitLossViewModel {
+        public string ProfitLossId { get; set; } = null!;
+        public string WoNum { get; set; } = null!;
+        public string VendorName { get; set; } = null!;
+        public decimal? Profit { get; set; }
+    }
+}
