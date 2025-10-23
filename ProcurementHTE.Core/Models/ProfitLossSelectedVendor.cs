fcf998bdd8d0e9a6cf4e87083ac1d1997bc2cc31
bryankazuro04 <mhdalana211201@gmail.com>
2025-10-24 00:32:50 +0700

    feat(profitlossselectedvendorid): create new table to store selected vendors for ProfitLoss
    Added a new model and corresponding database table to store selected vendors associated with each
    ProfitLoss record, enabling better data linkage and management of vendor selections in profit-loss
    operations

---
+++ ProcurementHTE.Core/Models/ProfitLossSelectedVendor.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcurementHTE.Core.Models
+{
+    public class ProfitLossSelectedVendor
+    {
+        public string ProfitLossSelectedVendorId { get; set; } = Guid.NewGuid().ToString();
+        public string WorkOrderId { get; set; } = null!;
+        public string VendorId { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
