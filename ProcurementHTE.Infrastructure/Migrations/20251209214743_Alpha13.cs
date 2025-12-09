79cdd52a5cbfad411cc749ff5cccd6297ac05e6b
bryankazuro04 <mhdalana211201@gmail.com>
2025-12-10 04:50:02 +0700

    feat(migrations): Add Alpha13 migration for schema updates
    This commit introduces a new migration named `Alpha13` that modifies the `Procurements` table schema. The `ProcurementCategory` column is changed from an `int` to a `string` type with a maximum length of 50 characters, and the `JobTypeId` column is updated to be non-nullable with a default value of an empty string. These changes ensure that the Entity Framework Core model is in sync with the updated database schema.

---
+++ ProcurementHTE.Infrastructure/Migrations/20251209214743_Alpha13.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProcurementHTE.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alpha13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProcurementCategory",
+                table: "Procurements",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobTypeId",
+                table: "Procurements",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProcurementCategory",
+                table: "Procurements",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobTypeId",
+                table: "Procurements",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
