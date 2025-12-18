a8b0789f4200003d2e696567c38b8d59f11f3842
bryankazuro04 <mhdalana211201@gmail.com>
2025-12-18 18:46:36 +0700

    style(IndonesianTerbilangHelper): Update number words to uppercase
    Replaced lowercase Indonesian number words in the `angkaDasar` array with their uppercase counterparts for consistency and adherence to style guidelines.

--- ProcurementHTE.Core/Utils/IndonesianTerbilangHelper.cs
+++ ProcurementHTE.Core/Utils/IndonesianTerbilangHelper.cs
@@ -118,17 +118,17 @@
             string[] angkaDasar =
             {
                 "", // dummy index 0 (tidak dipakai)
-                "satu", // 1
-                "dua", // 2
-                "tiga", // 3
-                "empat", // 4
-                "lima", // 5
-                "enam", // 6
-                "tujuh", // 7
-                "delapan", // 8
-                "sembilan", // 9
-                "sepuluh", // 10
-                "sebelas", // 11
+                "Satu", // 1
+                "Dua", // 2
+                "Tiga", // 3
+                "Empat", // 4
+                "Lima", // 5
+                "Enam", // 6
+                "Tujuh", // 7
+                "Delapan", // 8
+                "Sembilan", // 9
+                "Sepuluh", // 10
+                "Sebelas", // 11
             };
 
             string result;
